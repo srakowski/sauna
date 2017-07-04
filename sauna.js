20afea4f629ed32a491f973925c9307b8f4e5994
sebd-davra <sebd-davra@users.noreply.github.com>
2017-07-04 11:47:44 +0100

    fix if view is null

--- sauna.js
+++ sauna.js
@@ -1,51 +1,61 @@
 (function () {
 
-	var fs = require('fs');
-	var path = require('path');
-	var rootDir = path.dirname(require.main.filename) + "\\";
+    var fs = require('fs');
+    var path = require('path');
+    var rootDir = path.dirname(require.main.filename) + path.sep;
 
-	var Navigator = function (element) {
-		var self = this;
-		var _element = element;
-		var _v = null;
-		var _cb = null;
+    var Navigator = function (element) {
+        var self = this;
+        var _element = element;
+        var _v = null;
+        var _cb = null;
 
-		self.navigate = function (view, callback) {
-			_cb = callback;
-			location.hash = "#" + view;
-		};
+        self.navigate = function (view, callback) {
+            _cb = callback;
+            if ((view !=null) && (view.length > 0)) {
+                location.hash = "#" + view;
+            }
 
-		window.addEventListener("hashchange", function () {
-			var hash = location.hash;
-			var viewName = hash.slice(1, hash.length);
-			_navigate(viewName); 
-		}, false);
+        };
 
-		var _navigate = function (view) {
-			fs.readFile(rootDir + view + ".html", "utf-8", function (err, data) {
-				if (_v != null) {
-					ko.removeNode(_v);
-					_v = null;
-				}
-				element.innerHTML = data;
-				_v = document.getElementById(view)
-				var vmModule = _v.getAttribute("data-vm");
-				VM = require(rootDir + vmModule);
-				VM = new VM();
-				ko.applyBindings(VM, _v);
-				if (typeof _cb == 'function') {
-					_cb(VM);
-					_cb = null;
-				}
-			});
-		}
-	};
+        window.addEventListener("hashchange", function () {
+            var hash = location.hash;
+            var viewName = hash.slice(1, hash.length);
+            _navigate(viewName);
+        }, false);
 
-	module.exports = function (element) {
-		var navigator = new Navigator(element);
-		return {
-			n: navigator
-		};
-	};
+        var _navigate = function (view) {
+            if ((view !=null) && (view.length > 0)) {
+                fs.readFile(rootDir + path.sep + view + ".html", "utf-8", function (err, data) {
+                    console.log(rootDir + path.sep + view + ".html");
+                    if (_v != null) {
+                        ko.removeNode(_v);
+                        _v = null;
+                    }
+                    element.innerHTML = data;
+                    _v = document.getElementById(view)
+                    console.log(view);
+                    if (_v != null) {
+                        var vmModule = _v.getAttribute("data-vm");
+                        VM = require(rootDir + vmModule);
+                        VM = new VM();
+                        ko.applyBindings(VM, _v);
+                        if (typeof _cb == 'function') {
+                            _cb(VM);
+                            _cb = null;
+                        }
+                    }
+                });
+            }
+
+        }
+    };
+
+    module.exports = function (element) {
+        var navigator = new Navigator(element);
+        return {
+            n: navigator
+        };
+    };
 
 })();
